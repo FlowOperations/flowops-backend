7311100e81dc9dbbe8440130c248a8ba3134650b
rafadevgg <rafaelpc5671@gmail.com>
2026-09-19 19:13:22 -0300

    feat: Criação da arquitetura inicial do back-end

---
+++ src/main/java/br/com/flowopsbackend/FlowopsBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.flowopsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowopsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowopsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/flowopsbackend/FlowopsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flowopsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowopsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
